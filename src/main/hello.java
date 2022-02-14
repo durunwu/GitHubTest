3725e2558f9c8d574398eebd8c80702f1d48395c
runwu <runwu@123.com>
2022-02-14 13:55:17 +0800

    push test commit

--- src/main/hello.java
+++ src/main/hello.java
@@ -3,5 +3,7 @@ package main;
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello.txt");
+        System.out.println("hello.txt");
+        System.out.println("aaaaaaaaaaaaaaaaaaa");
     }
 }
